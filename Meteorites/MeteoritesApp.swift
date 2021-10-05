5ee6d5fbcfb7730ed7a59665888c9dd57b5e9f4f
Tomáš Šmerda <xsmerda@mendelu.cz>
2021-10-05 17:16:50 +0200

    Initial Commit

---
+++ Meteorites/MeteoritesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoritesApp.swift
+//  Meteorites
+//
+//  Created by Tomáš Šmerda on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeteoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
